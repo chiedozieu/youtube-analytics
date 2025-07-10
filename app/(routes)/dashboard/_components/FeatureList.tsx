858c8294d20397a4fbb1975712e791b728515830
chiedozieu <chiedozieugwor@gmail.com>
2025-07-10 13:47:34 -0400

    Refactor code structure for improved readability and maintainability

--- app/(routes)/dashboard/_components/FeatureList.tsx
+++ app/(routes)/dashboard/_components/FeatureList.tsx
@@ -7,44 +7,44 @@ function FeatureList() {
     {
       id: 1,
       title: "AI-Thumbnail Generator",
-      image: "/KeneGrad.jpeg",
+      image: "/thumbgen.jpeg",
       path: "/ai-thumbnail-generator",
     },
     {
       id: 2,
       title: "AI-Thumbnail Search",
-      image: "/KeneGrad.jpeg",
+      image: "/thumsearch.jpeg",
       path: "#",
     },
     {
       id: 3,
       title: "AI-Content Generator",
-      image: "/KeneGrad.jpeg",
+      image: "/contentgen.jpeg",
       path: "#",
     },
     {
       id: 4,
       title: "Outlier",
-      image: "/KeneGrad.jpeg",
+      image: "/outliers.jpeg",
       path: "#",
     },
     {
       id: 5,
       title: "Keywords",
-      image: "/KeneGrad.jpeg",
+      image: "/thumsearch.jpeg",
       path: "#",
     },
     {
       id: 6,
       title: "Optimize Video",
-      image: "/KeneGrad.jpeg",
+      image: "/optimize.jpeg",
       path: "#",
     },
   ];
   return (
     <div className="mt-7">
       <h2 className="text-2xl font-bold">AI Tools</h2>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
         {Features.map((feature) => (
           <Link href={feature.path}>
             <Image
